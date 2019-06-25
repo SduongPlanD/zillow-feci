1019fb0916bb3db103e33b0c47a00743b25d5376
Steve Duong <steve@Plan.D>
2019-06-25 11:56:25 -0700

    enable ES6 and React code for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       loader: 'babel-loader',
-  //       query: {
-  //         presets: ['es2015']
-  //       }
-  //     }
-  //   ]
-  // },
-  stats: {
-    colors: true
-  },
-  devtool: 'source-map'
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 };
